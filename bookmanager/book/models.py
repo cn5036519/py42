04eb71ffc8e4812a246a66e58792af05e67a4227
lucy <lucy@163.com>
2020-09-24 12:42:06 +0800

    数据库操作(F和Q对象)

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -2,18 +2,39 @@ from django.db import models
 
 # Create your models here.
 class BookInfo(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, verbose_name="书名")
+    pub_date = models.DateField(null=True, verbose_name="出版日期")
+    readcount = models.IntegerField(default=0, verbose_name="阅读量")
+    commentcount = models.IntegerField(default=0, verbose_name="评论量")
+    is_delete = models.BooleanField(default=False, verbose_name="逻辑删除")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = "bookinfo"
+        verbose_name = "书籍"
 
 
 class PeopleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    gender = models.BooleanField(default=True)
-    book = models.ForeignKey(to=BookInfo, on_delete=models.CASCADE)
+    GENDER_CHOICES = (
+        ('1', "男"),
+        ('2', "女"),
+        ('3', "未知"),
+    )
+    name = models.CharField(max_length=10, verbose_name="姓名")
+    # smallint
+    gender = models.SmallIntegerField(choices=GENDER_CHOICES, default=3, verbose_name="性别")
+    description = models.CharField(max_length=100, null=True, verbose_name="描述信息")
+    # tinyint
+    is_delete = models.BooleanField(default=False, verbose_name="逻辑删除")
+    book = models.ForeignKey(to=BookInfo, on_delete=models.CASCADE, verbose_name="书籍")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = "peopleinfo"
+        verbose_name = "人物"
 
 
--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,10 +1,53 @@
+from django.db.models import F, Q
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from book.models import BookInfo
+
+
 def index(request):
     return HttpResponse("ok")
 
 def center(request):
     # 将上下文交给模板进行处理,处理后将视图响应给客户端.
     return render(request, "book/center.html", {'title': "center页面"})
+
+
+if __name__ == '__main__':
+
+    # 查询编号为1的图书
+    BookInfo.objects.get(id=1)
+    # 查询书名包含'湖'的图书
+    BookInfo.objects.filter(name__contains="湖")
+    # 查询书名以'部'结尾的图书
+    BookInfo.objects.filter(name__endswith="部")
+    # 查询书名为空的图书
+    BookInfo.objects.filter(name__isnull=True)
+    # 查询编号为1或3或5的图书
+    BookInfo.objects.filter(id__in=(1,3,5))
+    BookInfo.objects.filter(id__in=[1,3,5])
+    # 查询编号大于3的图书
+    BookInfo.objects.filter(pk__gt=3)
+    # 查询1980年发表的图书
+    BookInfo.objects.filter(pub_date__year="1980")
+    # 查询1990年1月1日后发表的图书
+    BookInfo.objects.filter(pub_date__gt="1990-1-1")
+    BookInfo.objects.filter(pub_date__gt="1990-01-01")
+
+    # 查询阅读量大于等于评论量的图书
+    BookInfo.objects.filter(readcount__gte=F("commentcount"))
+    # 查询阅读量大于2倍评论量的图书
+    BookInfo.objects.filter(readcount__gt=F("commentcount")*2)
+    # 查询阅读量大于20，并且编号小于3的图书
+    BookInfo.objects.filter(readcount__gt=20, id__lt=3)
+    BookInfo.objects.filter(readcount__gt=20).filter(id__lt=3)
+    BookInfo.objects.filter(Q(readcount__gt=20)&Q(pk__lt=3))
+    # 查询阅读量大于20的图书
+    BookInfo.objects.filter(readcount__gt=20)
+    # 查询阅读量大于20，或编号小于3的图书
+    BookInfo.objects.filter(Q(readcount__gt=20)|Q(pk__lt=3))
+    # 查询编号不等于3的图书
+    BookInfo.objects.exclude(id=3)
+    BookInfo.objects.filter(~Q(id=3))
+    BookInfo.objects.filter(Q(id__gt=3)|Q(id__lt=3))
